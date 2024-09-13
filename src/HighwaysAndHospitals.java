7a3c788aa1207f67e19b795fb0b9c376e82f5ce0
stefanperkovic <stefan.perkovic@menloschool.org>
2024-09-12 18:16:09 -0700

    Started the union find algorithm, map roots to their connections.

--- src/HighwaysAndHospitals.java
+++ src/HighwaysAndHospitals.java
@@ -19,44 +19,22 @@ public class HighwaysAndHospitals {
     public static long cost(int n, int hospitalCost, int highwayCost, int cities[][]) {
 
         if (highwayCost >= hospitalCost){
-            return hospitalCost * cities.length;
+            return hospitalCost * n;
         }
-
-
-        boolean[][] connections = new boolean[n + 1][n + 1];
-        for (int i = 0; i < cities.length; i++){
-            int start = cities[i][0];
-            int end = cities[i][1];
-            connections[start][end] = true;
-            connections[end][start] = true;
-        }
-
-        boolean[] visited = new boolean[n + 1];
-        Stack<Integer> stack = new Stack<Integer>();
-        ArrayList<Long> sub_trees = new ArrayList<>(Long);
-        for (int i = 1; i <= n; i++){
-            if(!visited[i]){
-                stack.push(i);
-                visited[i] = true;
-                while (!stack.isEmpty()){
-                    int city = stack.pop();
-
-                    for (int j = i; j <= n; j++){
-                        if (connections[city][j]){
-                            if (!visited[j]){
-                                stack.push(j);
-                                visited[j] = true;
-                            }
-                        }
-                    }
-                }
-
-
+        int[] roots = new int[n + 1];
+        for (int i = 0; i < cities[0].length; i++){
+            if (roots[cities[i][0]] != roots[cities[i][1]]){
+                roots[cities[i][1]] = cities[i][0];
             }
+
         }
 
 
-        return 0;
+
+
+
+
+
     }
 
 }
