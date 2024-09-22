66ec1a48492357b93f0c5d2bb05d2ec4b96f0d7f
stefanperkovic <stefan.perkovic@menloschool.org>
2024-09-22 14:06:01 -0700

    Shortened up path compression and changed subgraph counting.

--- src/HighwaysAndHospitals.java
+++ src/HighwaysAndHospitals.java
@@ -17,48 +17,42 @@ public class HighwaysAndHospitals {
 
 
     public static long cost(int n, int hospitalCost, int highwayCost, int cities[][]) {
-
+        // If highways are more expensive or equal to hospitals, just build hospitals in every city
         if (highwayCost >= hospitalCost){
+            System.out.println((long) hospitalCost * n);
             return (long) hospitalCost * n;
         }
         int[] roots = new int[n + 1];
         for (int i = 0; i < cities.length; i++){
             int city1 = cities[i][0];
             int city2 = cities[i][1];
-            int x = city1;
 
-            while (roots[x] != 0){
+            int x = city1;
+            while (roots[x] > 0){
+                roots[x] = roots[roots[x]];
                 x = roots[x];
-            }
-            while (roots[city1] != 0){
-                int temp = roots[city1];
-                roots[city1] = x;
-                city1 = temp;
             }
 
             int z = city2;
-            while (roots[z] != 0){
+            while (roots[z] > 0){
+                roots[z] = roots[roots[z]];
                 z = roots[z];
             }
-            while (roots[city2] != 0){
-                int temp = roots[city2];
-                roots[city2] = z;
-                city2 = temp;
-            }
 
 
-            if (city1 != city2){
-                roots[city2] = city1;
+            if (x != z){
+
+                roots[z] = x;
             }
 
         }
         int subgraphs = 0;
         for (int i = 1; i < roots.length; i++){
-            if (roots[i] == 0){
+            if (roots[i] <= 0){
                 subgraphs++;
             }
         }
-
+        System.out.println((long) highwayCost * (n - subgraphs) + (long)hospitalCost * subgraphs);
         return (long) highwayCost * (n - subgraphs) + (long)hospitalCost * subgraphs;
 
     }
