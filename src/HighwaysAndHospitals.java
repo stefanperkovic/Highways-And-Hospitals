4b44ab90ebcd03c97c0a37c2b1056ef45ced7f3d
stefanperkovic <stefan.perkovic@menloschool.org>
2024-09-22 22:35:18 -0700

    Created a function find so that I can re-use the find function. Change subgraph calculation.

--- src/HighwaysAndHospitals.java
+++ src/HighwaysAndHospitals.java
@@ -22,38 +22,21 @@ public class HighwaysAndHospitals {
             return (long) hospitalCost * n;
         }
         int[] roots = new int[n + 1];
+        int subgraphs = n;
         for (int i = 0; i < cities.length; i++) {
             int city1 = cities[i][0];
             int city2 = cities[i][1];
-            int root1 = city1;
 
-            while (roots[root1] != 0) {
-                root1 = roots[root1];
-            }
-            while (roots[city1] != 0) {
-                int temp = roots[city1];
-                roots[city1] = root1;
-                city1 = temp;
-            }
-            int root2 = city2;
-            while (roots[root2] != 0) {
-                root2 = roots[root2];
-            }
-            while (roots[city2] != 0) {
-                int temp = roots[city2];
-                roots[city2] = root2;
-                city2 = temp;
-            }
+            int root1 = find(city1, roots);
 
-            union(root1, root2, roots);
+            int root2 = find(city2, roots);
 
 
-        }
-        int subgraphs = 0;
-        for (int i = 1; i < roots.length; i++) {
-            if (roots[i] == 0) {
-                subgraphs++;
+            int result = union(root1, root2, roots);
+            if (result == 1) {
+                subgraphs--;
             }
+
         }
 
         return (long) highwayCost * (n - subgraphs) + (long) hospitalCost * subgraphs;
@@ -75,9 +58,24 @@ public class HighwaysAndHospitals {
                 roots[root1] += roots[root2] - 1;
                 roots[root2] = root1;
             }
+            return 1;
+        }
+        return 0;
+
+    }
+
+    public static int find(int city, int[] roots){
+        int bottom_root = city;
+        while (roots[bottom_root] > 0) {
+            bottom_root = roots[bottom_root];
         }
 
-
+        while (roots[city] > 0) {
+            int temp = roots[city];
+            roots[city] = bottom_root;
+            city = temp;
+        }
+        return bottom_root;
     }
 
 
