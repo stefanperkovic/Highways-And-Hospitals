1e7710716bafaece53e0efd8baf3568fd71a8819
stefanperkovic <stefan.perkovic@menloschool.org>
2024-09-22 22:04:13 -0700

    Put the union into a separate function. Realize I had just a mistype in the while loop causing error.

--- src/HighwaysAndHospitals.java
+++ src/HighwaysAndHospitals.java
@@ -17,62 +17,68 @@ public class HighwaysAndHospitals {
 
 
     public static long cost(int n, int hospitalCost, int highwayCost, int cities[][]) {
-        // If highways are more expensive or equal to hospitals, just build hospitals in every city
-        if (highwayCost >= hospitalCost){
-            System.out.println((long) hospitalCost * n);
+
+        if (highwayCost >= hospitalCost) {
             return (long) hospitalCost * n;
         }
         int[] roots = new int[n + 1];
-        for (int i = 0; i < cities.length; i++){
+        for (int i = 0; i < cities.length; i++) {
             int city1 = cities[i][0];
             int city2 = cities[i][1];
+            int root1 = city1;
 
-            int x = city1;
-            while (roots[x] != 0){
-
-                x = roots[x];
+            while (roots[root1] != 0) {
+                root1 = roots[root1];
             }
-            while (roots[city1] != 0){
+            while (roots[city1] != 0) {
                 int temp = roots[city1];
-                roots[city1] = x;
+                roots[city1] = root1;
                 city1 = temp;
             }
-
-            int z = city2;
-            while (roots[z] != 0){
-                z = roots[x];
+            int root2 = city2;
+            while (roots[root2] != 0) {
+                root2 = roots[root2];
             }
-            while (roots[city2] != 0){
+            while (roots[city2] != 0) {
                 int temp = roots[city2];
-                roots[city2] = x;
+                roots[city2] = root2;
                 city2 = temp;
             }
 
-            if (x != z){
-//                int a = roots[x];
-//                int b = roots[z];
-//                if (a >= b){
-//                    roots[x] += roots[z] - 1;
-//                    roots[z] = x;
-//                }
-//                else{
-//                    roots[z] += roots[x] - 1;
-//                    roots[x] = z;
-//                }
-                roots[z] = x;
+            union(root1, root2, roots);
 
-            }
 
         }
         int subgraphs = 0;
-        for (int i = 1; i < roots.length; i++){
-            if (roots[i] == 0){
+        for (int i = 1; i < roots.length; i++) {
+            if (roots[i] == 0) {
                 subgraphs++;
             }
         }
-        System.out.println((long) highwayCost * (n - subgraphs) + (long)hospitalCost * subgraphs);
-        return (long) highwayCost * (n - subgraphs) + (long)hospitalCost * subgraphs;
+
+        return (long) highwayCost * (n - subgraphs) + (long) hospitalCost * subgraphs;
+    }
+
+    public static int union(int root1, int root2, int[] roots){
+        if (root1 != root2) {
+            int a = roots[root1];
+            int b = roots[root2];
+            if (a < b){
+                roots[root1] += roots[root2];
+                roots[root2] = root1;
+            }
+            else if(a > b) {
+                roots[root2] += roots[root1];
+                roots[root1] = root2;
+            }
+            else{
+                roots[root1] += roots[root2] - 1;
+                roots[root2] = root1;
+            }
+        }
+
 
     }
+
 
 }
